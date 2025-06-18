26acf4e5f08775fa78e27c4741989fa9465a480d
Vadim Volk <volkvx@gmail.com>
2025-06-18 16:29:20 -0700

    Updates

---
+++ projects/css/vheaderl2.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+<div>\
+<a href="/"> <img src="images/ViladioDesignLogo.png" style="height: 46px; float:left; margin-top:10px; margin-left:12px"/></a>\
+<span class="right">\
+<a href="../index.html">cases</a>\
+<a href="../resume.html">resume</a>\
+<a href="../about.html">about</a>\
+</span>\
+</div>\
+\
+');
