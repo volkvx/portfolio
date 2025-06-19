46961860ac35aac6df99f6acb5510afba4ca757c
Vadim Volk <volkvx@gmail.com>
2025-06-19 10:23:48 -0700

    updating vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change this if deploying to a subfolder (e.g. GitHub Pages)
+  base: '/', // Change this if deploying to a subfolder (e.g. GitHub Pages)
 })
